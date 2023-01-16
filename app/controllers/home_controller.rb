89fd382d48701303197f304dbfb9b9ca74fbf623
Asad <iamasad128@gmail.com>
2023-01-16 15:38:34 +0800

    Add home controller and views along with filters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    # generate_mystery_lunch_partners
+    @mystery_partners = retreive_mystery_lunch_partners
+  end
+
+  private
+
+  def generate_mystery_lunch_partners
+    MysteryMatchGenerator.new.call
+  end
+
+  def retreive_mystery_lunch_partners
+    MysteryMatchRetrieval.new(department_id: params[:department_id].to_i, year: params[:year].to_i, month: params[:month].to_i).call
   end
 end
